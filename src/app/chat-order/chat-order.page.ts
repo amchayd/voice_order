f4ec9e2185e4573d85dd627ed0e6fde201af06e9
AMCHAYD Oualid <amchaydoualid@gmail.com>
2021-05-30 21:31:21 +0100

    Adding chat-order layout

--- src/app/chat-order/chat-order.page.ts
+++ src/app/chat-order/chat-order.page.ts
@@ -28,9 +28,102 @@ export class ChatOrderPage implements OnInit {
   constructor(private speechRecognition: SpeechRecognition, 
     private cd: ChangeDetectorRef, 
     private dbService: DbService,
-    private matcherService: MatcherService) { }
+    private matcherService: MatcherService) { 
+      this.categories = dbService.getCategories();
+      //this.startOrdering();
+    }
 
   ngOnInit() {
+  }
+
+  getpermission() {
+    this.speechRecognition.hasPermission()
+    .then((hasPermission: boolean) => {
+      if(!hasPermission) {
+        // Request permissions
+        this.speechRecognition.requestPermission()
+        .then(
+          () => console.log('Granted'),
+          () => console.log('Denied')
+        )
+      }
+    })
+  }
+
+  startRecord() {
+    
+    this.log+="- start recording";
+    this.getpermission();
+
+    let options = {
+      language: 'en-US'
+      //showPopup: false
+    }
+    this.speechRecognition.startListening(options).subscribe(
+      matches => {
+      this.response = matches[0];
+      this.verifyTheRecorderOrder(matches[0]);
+      this.cd.detectChanges();
+
+    }, error=>  {
+      this.log+="- Error when recording :"+error;
+    });
+    this.isRecording = true;
+    this.tmpStatus = "is recording...";
+    this.status = "mic";
+  }
+  stopRecord() {
+    this.speechRecognition.stopListening().then(()=>{
+      this.isRecording=false;
+    });
+  }
+
+  startOrdering() {
+    this.log+="- start the order"
+    if(!this.isRecording) {
+      this.botTalking();
+    } else {
+     // this.stopRecord();
+    }
+  }
+
+  botTalking() {
+    this.log+="- the bot is talking";
+    this.tmpStatus = "The bot is talking...";
+    this.status = "volume-high-outline";
+    this.botMessage = this.workflow[this.step];
+    this.startRecord();
+  }
+
+  verifyTheRecorderOrder(order: string) {
+    this.log+="- verifie the recorded order";
+    this.isRecording=false;
+    var max = 0;
+    var matchedOrder = null;
+    for (let m of this.categories) {
+      var currentMatch = this.matcherService.checkSimilarity(order.toLowerCase(), m.name);
+      if( currentMatch > max) {
+        max = currentMatch;
+        matchedOrder = m  ;
+      }
+    }
+
+    if(max > 0.20) {
+      this.tmpStatus = "Valid order...";
+      this.categorie = matchedOrder.name;
+      // go to the next step
+      this.botTalking();
+      this.step++;
+    } else {
+        // invalid order, re answer
+        this.tmpStatus = "Invalid order...";
+        this.invalidOrder();
+    }
+  }
+
+  invalidOrder() {
+    this.botMessage = "We could'nt understand what you want? please re order ?";
+    this.startRecord();
   }
 
 }
